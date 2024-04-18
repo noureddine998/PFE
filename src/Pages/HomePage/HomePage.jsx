44234e8ff0940e079293026a65a5fe8c9ec4cdc2
ayman naaimi <aymannaaimi@gmail.com>
2024-04-18 22:19:54 +0100

    removed some unssessary stuff

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import './style.css';
-
+import Navbar from '../../components/Navbar/Navbar';
+import LoginForm from '../../components/LoginUser/LoginUser';
 const HomePage = () => {
   return (
+    <div className="home-page-container">
+      <div className="content-area"></div>
     <header className="site-header clearfix">
- 
+      <Navbar/>
       <nav>
         <div className="logo">
           <h1>E-Vote</h1>
@@ -15,10 +18,14 @@ const HomePage = () => {
           <img src="img/home_page.png" alt="Home Page" />
         </div>
         <div className="rightside">
-    
         </div>
       </section>
     </header>
+      <div className="login-form-area">
+      <LoginForm />
+      </div>
+    </div>
+    
   );
 };
 
