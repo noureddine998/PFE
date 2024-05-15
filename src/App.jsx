fd4f8cdf845580ba6dec12400f16a38007d4cf42
ayman naaimi <aymannaaimi@gmail.com>
2024-05-15 20:27:28 +0100

    app jsx routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,26 @@ import AdminPage from './Pages/AdminPage/AdminPage';
 import DistrictsPage from './Pages/DistrictList/DistrictList';
  import CandidatesList from './Pages/CandidatesList/CandidatesList';
  import VoterList from './Pages/VoterList/VoterList';
-// import AddVoter from './components/AddVoters/AddVoter';
-// import Login from './components/LoginUser/Login/Login'; // Import the Login component
-// import Signup from './components/LoginUser/Singup/Singup'; // Import the Signup component
-// import VotingPage from './Pages/VotingPage/VotingPage';
+import AddVoter from './components/AddVoters/AddVoter';
+import Login from './components/LoginUser/Login/Login'; // Import the Login component
+import Signup from './components/LoginUser/Singup/Singup'; // Import the Signup component
+import VotingPage from './Pages/VotingPage/VotingPage';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          {/* 
+          
          
           <Route path='/AdminLogin' element={<AdminLogin />} />
-           */}
+          
            <Route path='/CreateDistrict' element={<CreateDistrict />} />
           <Route path='/DistrictList' element={<DistrictsPage />} />
           <Route path='/CandidatesList' element={<CandidatesList />} />
           <Route path='/adminAddCandidate' element={<AdminAddCandidate />} />
           <Route path='/AdminPage' element={<AdminPage />} />
-            {/* <Route path='/' element={<HomePage />} />
+            <Route path='/' element={<HomePage />} />
             
             <Route path='/VoterList' element={<VoterList />} />
             <Route path='/addVoter' element={<AddVoter />} />
@@ -37,7 +37,7 @@ function App() {
           
             <Route path='/login' element={<Login />} />
             <Route path='/signup' element={<Signup />} />
-            <Route path='/votingpage' element={<VotingPage />} /> */}
+            <Route path='/votingpage' element={<VotingPage />} />
         </Routes>
       </div>
     </BrowserRouter>
